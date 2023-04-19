adba17b1117d7f36c7cd23d3f6890fe19dd73025
João Carlos Silva Ribeiro <jocasiribeiro@hotmail.com>
2023-04-18 21:01:54 -0300

    Implementação de regras de negocios

--- QuickBuy.Dominio/Entidades/Entidade.cs
+++ QuickBuy.Dominio/Entidades/Entidade.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
     public abstract class Entidade
     {
+        public List<string> _mensagemValidacao { get; set; }
+        private List<string> mensagemValidacao 
+        {
+            get { return _mensagemValidacao ?? (_mensagemValidacao = new List<string>()); }
+        }
+
+            protected void LimparMensagensValidacao()
+        {
+            mensagemValidacao.Clear();
+        }
+        protected void AdicionarCritica(string mensagem)
+        {
+            mensagemValidacao.Add(mensagem);
+        }
+        
+        public abstract void Validate();
+        protected bool EhValido
+        {
+            get { return !mensagemValidacao.Any(); }
+        }
     }
 }
